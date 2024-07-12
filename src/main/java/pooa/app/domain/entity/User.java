45a04d9abc18a30f4d331067ea9c484dca012009
azevedo <azevedoantoniocicero@gmail.com>
2024-07-11 22:25:44 -0300

    Codigo cagado mas fe com fe

---
+++ src/main/java/pooa/app/domain/entity/User.java
@@ -0,0 +1,64 @@
+package pooa.app.domain.entity;
+
+public abstract class User extends Person{
+    private long userId;
+    private String username;
+    private String password;
+    private boolean isActivate;
+    private boolean isSuperUser;
+
+
+    public User(Long personId, Long userId) {
+		this.idUser= idUser;
+	}
+	
+	public User(Long idUser, String username, String password, boolean isActivate, boolean isSuperUser) {
+		this.idUser= idUser;
+		this.username = username;
+		this.password = password;
+        this.isSuperUser = isSuperUser;
+        this.isActivate = isActivate;
+	}
+
+	public User(String username, String password, boolean isActivate, boolean isSuperUser) {
+		this.username = username;
+		this.password = password;
+        this.isSuperUser = isSuperUser;
+        this.isActivate = isActivate;
+	}
+	
+    public long getIdUser() {
+        return this.idUser;
+    }
+    public void setIdUser(long idUser) {
+        this.idUser= idUser;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+    public void setPassword(String password) {
+        this.password= password;
+    }
+
+    public boolean getIsActivate() {
+        return this.isActivate;
+    }
+    public void setIsActivate(boolean isActivate) {
+        this.isActivate = isActivate;
+    }
+
+    public boolean getIsSuperUser() {
+        return this.isSuperUser;
+    }
+    public void setIsSuperUser(boolean isSuperUser) {
+        this.isSuperUser = isSuperUser;
+    }
+}
